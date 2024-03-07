4a7310cd2ecc65169b5f04130567b1b6e02bcf18
Harshalmodi17 <harshal.modi@avantika.edu.in>
2024-03-07 11:04:01 +0530

    setup gh-pages

---
+++ src/Components/UI.jsx
@@ -0,0 +1,281 @@
+import styles from "./UI.module.css";
+
+const UI = () => {
+  return (
+    <div className={styles.ui}>
+      <div className={styles.divlwbaav} />
+      <div className={styles.header} />
+      <div className={styles.header1} />
+      <img className={styles.headerSection} alt="" src="/header--section.svg" />
+      <div className={styles.headerSection1}>BRAUSS</div>
+      <div className={styles.headerSection2}>Home</div>
+      <div className={styles.headerSection3}>Book Online</div>
+      <div className={styles.main}>
+        <div className={styles.section}>
+          <div className={styles.divlwbaav1} />
+        </div>
+        <div className={styles.section1}>
+          <div className={styles.section2}>
+            <div className={styles.heading1}>
+              <div className={styles.discoverYourFutureContainer}>
+                <span className={styles.discoverYourFutureContainer1}>
+                  <p className={styles.discover}>DISCOVER</p>
+                  <p className={styles.discover}>YOUR</p>
+                  <p className={styles.discover}>FUTURE</p>
+                </span>
+              </div>
+            </div>
+            <div className={styles.pfont6}>
+              <div className={styles.empoweringStudents}>
+                Empowering Students
+              </div>
+            </div>
+            <img
+              className={styles.wowImage11062b4543303ee22}
+              alt=""
+              src="/wowimage--11062b-4543303ee22144108e143744f4798a3dmv2jpg@2x.png"
+            />
+          </div>
+          <div className={styles.section3}>
+            <div className={styles.divlwbaav1} />
+            <img
+              className={styles.wixBgMediaWowImage110}
+              alt=""
+              src="/wixbgmedia--wowimage--11062b-3e5a2182e8cd4819a5d025c456ad19c7mv2jpg@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.section4}>
+          <div className={styles.divlwbaav1} />
+        </div>
+        <div className={styles.section5}>
+          <div className={styles.section6}>
+            <div className={styles.heading2}>BOOK A SERVICE</div>
+          </div>
+          <div className={styles.divsrcfhff}>
+            <div className={styles.divs6rfht3}>
+              <div className={styles.nothingToBook}>
+                Nothing to book right now. Check back soon.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.section7}>
+          <div className={styles.divlwbaav1} />
+        </div>
+        <div className={styles.section8}>
+          <div className={styles.section9}>
+            <div className={styles.heading21}>
+              <div className={styles.aboutUs}>ABOUT US</div>
+            </div>
+            <div className={styles.pfont8}>
+              <div className={styles.braussIsAContainer}>
+                <span className={styles.braussIsAContainer1}>
+                  <p className={styles.discover}>
+                    BRAUSS is a website that provides students, job
+                  </p>
+                  <p className={styles.discover}>
+                    seekers, educators, and individuals interested in
+                  </p>
+                  <p className={styles.discover}>
+                    government schemes and trending news with
+                  </p>
+                  <p className={styles.discover}>
+                    information about recruitment, admit cards, answer
+                  </p>
+                  <p className={styles.discover}>
+                    keys, results, and other updates for various
+                  </p>
+                  <p className={styles.discover}>
+                    universities and colleges. Our enriched learning
+                  </p>
+                  <p className={styles.discover}>
+                    environment reflects the diverse backgrounds and
+                  </p>
+                  <p className={styles.discover}>
+                    cultures of the area. Give us a call to find out more
+                  </p>
+                  <p className={styles.discover}>
+                    about how we can help you prepare for the future.
+                  </p>
+                </span>
+              </div>
+            </div>
+            <div className={styles.button}>
+              <div className={styles.before} />
+              <div className={styles.after} />
+              <div className={styles.learnMore}>Learn More</div>
+            </div>
+          </div>
+          <img className={styles.sectionIcon} alt="" src="/section@2x.png" />
+        </div>
+        <div className={styles.section10}>
+          <div className={styles.divlwbaav1} />
+        </div>
+        <div className={styles.section11}>
+          <div className={styles.section12}>
+            <div className={styles.heading22}>OUR SPECIALTIES</div>
+          </div>
+          <div className={styles.section13}>
+            <div className={styles.div}>
+              <img className={styles.frameIcon} alt="" src="/frame.svg" />
+              <div className={styles.heading3}>
+                <div className={styles.recruitmenTUpdatesContainer}>
+                  <span className={styles.braussIsAContainer1}>
+                    <p className={styles.discover}>RECRUITMEN</p>
+                    <p className={styles.discover}>T UPDATES</p>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.div1}>
+              <div className={styles.stayUpToContainer}>
+                <span className={styles.braussIsAContainer1}>
+                  <p className={styles.discover}>
+                    Stay up to date with the latest recruitment
+                  </p>
+                  <p className={styles.discover}>
+                    updates from various universities and
+                  </p>
+                  <p className={styles.discover}>
+                    colleges. BRAUSS provides comprehensive
+                  </p>
+                  <p className={styles.discover}>
+                    information on job notifications, online
+                  </p>
+                  <p className={styles.discover}>
+                    registration, paying fees online, previous
+                  </p>
+                  <p className={styles.discover}>
+                    year question papers, and syllabus.
+                  </p>
+                </span>
+              </div>
+            </div>
+            <div className={styles.div2}>
+              <div className={styles.button1}>
+                <div className={styles.before} />
+                <div className={styles.after} />
+                <div className={styles.learnMore}>Learn More</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.divcompLsssb85r2} />
+          <div className={styles.section14}>
+            <div className={styles.div3}>
+              <img className={styles.frameIcon} alt="" src="/frame.svg" />
+              <div className={styles.heading31}>
+                <div className={styles.admitCardsAndContainer}>
+                  <span className={styles.braussIsAContainer1}>
+                    <p className={styles.discover}>ADMIT</p>
+                    <p className={styles.discover}>CARDS AND</p>
+                    <p className={styles.discover}>RESULTS</p>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.div4}>
+              <div className={styles.braussProvidesComprehensiveContainer}>
+                <span className={styles.braussIsAContainer1}>
+                  <p className={styles.discover}>
+                    BRAUSS provides comprehensive
+                  </p>
+                  <p className={styles.discover}>
+                    information on admit cards and results for
+                  </p>
+                  <p className={styles.discover}>
+                    various universities and colleges. Stay
+                  </p>
+                  <p className={styles.discover}>
+                    updated with the latest news and
+                  </p>
+                  <p className={styles.discover}>notifications.</p>
+                </span>
+              </div>
+            </div>
+            <div className={styles.div5}>
+              <div className={styles.button2}>
+                <div className={styles.before} />
+                <div className={styles.after} />
+                <div className={styles.learnMore}>Learn More</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.divcompLsssb86a} />
+          <div className={styles.section15}>
+            <div className={styles.div3}>
+              <img className={styles.frameIcon} alt="" src="/frame.svg" />
+              <div className={styles.heading32}>
+                <div className={styles.previousYearQuestionContainer}>
+                  <span className={styles.discoverYourFutureContainer1}>
+                    <p className={styles.discover}>PREVIOUS</p>
+                    <p className={styles.discover}>YEAR</p>
+                    <p className={styles.discover}>QUESTION</p>
+                    <p className={styles.discover}>PAPERS</p>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.div7}>
+              <div className={styles.getAccessToContainer}>
+                <span className={styles.braussIsAContainer1}>
+                  <p className={styles.discover}>
+                    Get access to previous year question papers
+                  </p>
+                  <p className={styles.discover}>
+                    and syllabus for various universities and
+                  </p>
+                  <p className={styles.discover}>colleges. BRAUSS provides a</p>
+                  <p className={styles.discover}>
+                    comprehensive range of services to help
+                  </p>
+                  <p className={styles.discover}>you prepare for your exams.</p>
+                </span>
+              </div>
+            </div>
+            <div className={styles.div5}>
+              <div className={styles.button2}>
+                <div className={styles.before} />
+                <div className={styles.after} />
+                <div className={styles.learnMore}>Learn More</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer} />
+      <div className={styles.footer1} />
+      <div className={styles.footerSection}>BRAUSS</div>
+      <div className={styles.footerSectionContainer}>
+        <span className={styles.discoverYourFutureContainer1}>
+          <p className={styles.discover}>123-456-7890</p>
+          <p className={styles.discover}>info@mysite.com</p>
+        </span>
+      </div>
+      <div className={styles.footerSection1}>
+        000 ABC Street, XY City, PQ Country, 000000
+      </div>
+      <div className={styles.footerSectionContainer2}>
+        <span className={styles.braussIsAContainer1}>
+          <p className={styles.discover}>Stay informed, join our</p>
+          <p className={styles.discover}>newsletter</p>
+        </span>
+      </div>
+      <div className={styles.footerSectionForm}>
+        <div className={styles.label}>
+          <div className={styles.enterYourEmail}>Enter your email here *</div>
+        </div>
+        <div className={styles.input}>
+          <div className={styles.divplaceholder} />
+        </div>
+      </div>
+      <div className={styles.footerSectionFormButt}>
+        <div className={styles.before4} />
+        <div className={styles.after4} />
+        <div className={styles.submit}>Submit</div>
+      </div>
+    </div>
+  );
+};
+
+export default UI;
